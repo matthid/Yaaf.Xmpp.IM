4e4e56cd05956d7711194b5b5264a2f9e3d51469
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-03 21:00:19 +0100

    fix solution configuration.

--- src/SolutionInfo.fs
+++ src/SolutionInfo.fs
@@ -1,13 +1,13 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyCompanyAttribute("Yaaf.Xmpp.Runtime")>]
-[<assembly: AssemblyProductAttribute("Yaaf.Xmpp.Runtime")>]
-[<assembly: AssemblyCopyrightAttribute("Yaaf.Xmpp.Runtime Copyright © Matthias Dittrich 2015")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.2")>]
+[<assembly: AssemblyCompanyAttribute("Yaaf.Xmpp.IM")>]
+[<assembly: AssemblyProductAttribute("Yaaf.Xmpp.IM")>]
+[<assembly: AssemblyCopyrightAttribute("Yaaf.Xmpp.IM Copyright © Matthias Dittrich 2015")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] Version = "0.0.1"
